e0dd2bd4264625482448d335afe5b9a44f7b1f44
ala72295 <Alan.Snow@spectrumhealth.org>
2017-06-22 14:49:37 -0400

    Adding all initial files from my previous separate projects

---
+++ RestTemplateProjects/RestAPI/Controllers/StatusController.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace RestAPI.Controllers
+{
+    using System.Web.Http;
+
+    /// <summary>
+    /// Status Controller To Verify Endpoint Status
+    /// </summary>
+    /// <seealso cref="System.Web.Http.ApiController" />
+    [RoutePrefix("api")]
+    public class StatusController : ApiController
+    {
+        /// <summary>
+        /// Gets the status.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("status")]
+        public IHttpActionResult GetStatus()
+        {
+            var response = "RestAPI v0.1";
+
+            return this.Ok(response);
+        }
+    }
+}
---
+++ Template Source/Api/IoC.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IoC.cs">
+//  Copyright (c) 2017 All Rights Reserved
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace $safeprojectname$
+{
+    using Autofac;
+
+    /// <summary>
+    /// Inversion of Control Configuration
+    /// </summary>
+    public class IoC
+    {
+        /// <summary>
+        /// Registers the specified builder.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        internal static void Register(ContainerBuilder builder)
+        {
+            // Core
+
+            // Services
+
+            // Repositories
+
+        }
+
+    }
+}
