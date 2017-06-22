e695e529f50040193e9d71efc5671ec8966a6b17
ala72295 <Alan.Snow@spectrumhealth.org>
2017-06-22 14:49:37 -0400

    Adding all initial files from my previous separate projects

---
+++ RestTemplateProjects/RestAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Global.asax.cs">
+//  Copyright (c) 2017 All Rights Reserved
+// <author>Alan Snow</author>
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace RestAPI
+{
+    using System.Web.Http;
+
+    /// <summary>
+    /// Web API Configuration
+    /// </summary>
+    public static class WebApiConfig
+    {
+        /// <summary>
+        /// Registers the specified configuration.
+        /// </summary>
+        /// <param name="config">The configuration.</param>
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ RestTemplateProjects/RestAPI/Global.asax.cs
@@ -0,0 +1,57 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Global.asax.cs">
+//  Copyright (c) 2017 All Rights Reserved
+// <author>Alan Snow</author>
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace RestAPI
+{
+    using System.Reflection;
+    using System.Web;
+    using System.Web.Http;
+    using Autofac;
+    using Autofac.Integration.WebApi;
+    using AutoMapper;
+    using Newtonsoft.Json.Serialization;
+
+    /// <summary>
+    /// Startup Application Class
+    /// </summary>
+    public class WebApiApplication : HttpApplication
+    {
+        /// <summary>
+        /// Applications the start.
+        /// </summary>
+        protected void Application_Start()
+        {
+            var builder = new ContainerBuilder();
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            var config = GlobalConfiguration.Configuration;
+
+            // Register Auto Mapper Profiles
+            Mapper.Initialize(
+                cfg => cfg.AddProfiles(new[]
+                {
+                    "RestAPI",
+                }));
+
+            // Enable cammel case for response fields
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterWebApiFilterProvider(config);
+
+            IoC.Register(builder);
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
